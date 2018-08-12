6f378535a101e2f4301c03f4d728c68703d566db
Rishi Srivastava <rishisrivasta@gmail.com>
2018-08-13 05:57:34 +0800

    my first python code

---
+++ quantra/MyFirstPythonCode.py
@@ -0,0 +1,141 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[2]:
+
+
+print("I am new to Python")
+print("Python is cool!")
+
+
+# In[3]:
+
+
+x = 2
+y = 3
+sum = x + y
+print(sum)
+
+
+# In[4]:
+
+
+x = 100
+y = 50
+
+
+# In[5]:
+
+
+id(x)
+
+
+# In[6]:
+
+
+id(y)
+
+
+# In[7]:
+
+
+type(x)
+
+
+# In[8]:
+
+
+type(y)
+
+
+# In[9]:
+
+
+x = x + 1.11
+print(x)
+type(x)
+
+
+# In[10]:
+
+
+id(x)
+
+
+# In[11]:
+
+
+x = "hundred"
+print(x)
+type(x)
+
+
+# In[12]:
+
+
+id(x)
+
+
+# In[13]:
+
+
+a = 123
+
+
+# In[14]:
+
+
+b = 123
+
+
+# In[15]:
+
+
+id(a)
+
+
+# In[16]:
+
+
+id(b)
+
+
+# In[1]:
+
+
+biology_marks = 82
+physics_marks = 91
+maths_marks = 96
+chemistry_marks = 88
+total_marks = biology_marks + physics_marks + maths_marks + chemistry_marks
+print(total_marks)
+
+
+# In[2]:
+
+
+total_marks = biology_marks + physics_marks + maths_marks + chemistry_marks
+print(total_marks)
+
+
+# In[3]:
+
+
+num = 8
+   num_sq = num ** 2
+print(num_sq)
+
+
+# In[4]:
+
+
+num = 8
+num_sq = num ** 2
+print(num_sq)
+
