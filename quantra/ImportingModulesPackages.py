8438a46e52daa902de1944e4d7de3022c6c8160b
Rishi Srivastava <rishisrivasta@gmail.com>
2018-08-14 05:55:24 +0800

    added import demo

---
+++ quantra/ImportingModulesPackages.py
@@ -0,0 +1,104 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+
+
+# In[2]:
+
+
+math.pi
+
+
+# In[3]:
+
+
+math.cos(1)
+
+
+# In[4]:
+
+
+math.sin(1)
+
+
+# In[5]:
+
+
+dir(math)
+
+
+# In[6]:
+
+
+from scipy import mean
+
+
+# In[7]:
+
+
+mean([1,2,3,4,5])
+
+
+# In[8]:
+
+
+from scipy import stats
+
+
+# In[9]:
+
+
+stats.hmean([1,2,3,4,5])
+
+
+# In[10]:
+
+
+from numpy import *
+
+
+# In[11]:
+
+
+sin(1)
+
+
+# In[12]:
+
+
+diag([1,5,9,6])
+
+
+# In[13]:
+
+
+import numpy as np
+
+
+# In[14]:
+
+
+dir(np)
+
+
+# In[15]:
+
+
+np.median([4,5,6,3,4,5,9,8,7,12])
+
+
+# In[16]:
+
+
+np.min([4,5,6,3,4,5,9,8,7,12])
+
+
+# In[17]:
+
+
+np.max([4,5,6,3,4,5,9,8,7,12])
+
